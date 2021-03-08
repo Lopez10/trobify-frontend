590eb0321acd2e516510405a92a206d503e97027
Juan <ljuan7397@gmail.com>
2021-03-08 19:18:13 +0100

    eliminando cosas innecesarias

--- src/catalogo/index.catalogo.ts
+++ src/catalogo/index.catalogo.ts
@@ -4,7 +4,7 @@ export class catalogo {
 	constructor() {
 		this.getCatalogo();
 	}
-	async getCatalogo() {
+	getCatalogo() {
 		const myRequest = 'http://localhost:3000/catalogo';
 		let listadoInmuebles = axios.get(myRequest);
 		return listadoInmuebles;
