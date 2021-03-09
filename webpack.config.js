14f03a9e28d6be6e90a351260c760c5437b8f1f6
Juan <ljuan7397@gmail.com>
2021-03-09 17:47:03 +0100

    nuevo modulo

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.ts',
+	plugins: [new HtmlWebpackPlugin({ title: 'Development' })],
 	mode: 'development',
 	module: {
 		rules: [
