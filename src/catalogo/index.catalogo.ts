93492d0f05375ad7c0de4d7c35bc006c3ec7dd96
Juan <ljuan7397@gmail.com>
2021-03-13 19:57:41 +0100

    creacion de dom dinamico

--- src/catalogo/index.catalogo.ts
+++ src/catalogo/index.catalogo.ts
@@ -22,9 +22,38 @@ export class Catalogo {
 		const div = document.getElementById('catalogo');
 		catalogo.then((result) => {
 			result.forEach((item: any) => {
-				const p = document.createElement('p');
-				p.textContent = item.tipo;
-				div?.appendChild(p);
+				let property = document.createElement('div');
+				property.className = 'property_item';
+				let content = document.createElement('div');
+				content.className = 'content';
+				let price = document.createElement('div');
+				price.className = 'price';
+				let priceTitle = document.createElement('div');
+				priceTitle.className = 'label title-x2';
+				let priceText = document.createElement('p');
+				priceText.textContent = item.precio;
+				priceTitle.appendChild(priceText);
+				price.appendChild(priceTitle);
+				content.appendChild(price);
+
+				let location = document.createElement('div');
+				location.className = 'location';
+				let labelBold = document.createElement('div');
+				labelBold.className = 'label bold';
+				labelBold.textContent = 'Ubicacion del inmueble';
+				location.appendChild(labelBold);
+				content.appendChild(location);
+
+				let features = document.createElement('div');
+				features.className = 'features';
+				let labelBoldBanos = document.createElement('div');
+				labelBoldBanos.className = 'label bold';
+				labelBoldBanos.textContent = item.banos;
+				features.appendChild(labelBoldBanos);
+				content.appendChild(features);
+
+				property.appendChild(content);
+				div?.appendChild(property);
 			});
 		});
 	}
