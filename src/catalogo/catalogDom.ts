8197ec82c04b377390f2f1a766f9f998da52abbc
Juan <ljuan7397@gmail.com>
2021-03-13 20:09:07 +0100

    distribucion del dom

---
+++ src/catalogo/catalogDom.ts
@@ -0,0 +1,34 @@
+export function catalogDom(div: any, item: any) {
+	let property = document.createElement('div');
+	property.className = 'property_item';
+	let content = document.createElement('div');
+	content.className = 'content';
+	let price = document.createElement('div');
+	price.className = 'price';
+	let priceTitle = document.createElement('div');
+	priceTitle.className = 'label title-x2';
+	let priceText = document.createElement('p');
+	priceText.textContent = item.precio;
+	priceTitle.appendChild(priceText);
+	price.appendChild(priceTitle);
+	content.appendChild(price);
+
+	let location = document.createElement('div');
+	location.className = 'location';
+	let labelBold = document.createElement('div');
+	labelBold.className = 'label bold';
+	labelBold.textContent = 'Ubicacion del inmueble';
+	location.appendChild(labelBold);
+	content.appendChild(location);
+
+	let features = document.createElement('div');
+	features.className = 'features';
+	let labelBoldBanos = document.createElement('div');
+	labelBoldBanos.className = 'label bold';
+	labelBoldBanos.textContent = item.banos;
+	features.appendChild(labelBoldBanos);
+	content.appendChild(features);
+
+	property.appendChild(content);
+	div?.appendChild(property);
+}
