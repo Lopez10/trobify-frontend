ef38f26c5644512024b1415e36a3e22f8f6090b1
Juan <ljuan7397@gmail.com>
2021-03-18 09:14:43 +0100

    eliminacion metodo sobrante de catalogo y distribución

--- src/catalogo/index.catalogo.ts
+++ src/catalogo/index.catalogo.ts
@@ -1,39 +1,20 @@
 import axios from 'axios';
 import { catalogDom } from './catalogDom';
 export class Catalogo {
-	constructor() {
-		this.getCatalogo();
-	}
-	async getCatalogo() {
+	constructor() {}
+	async getCatalogo(filtros: any) {
+		console.log(filtros);
 		const myRequest = 'http://localhost:3000/catalogo';
 		let inmuebles: Promise<any> = await axios
 			.get(myRequest, {
 				params: {
-					opt: 1,
-					ord: 1,
-					preMin: 1,
-					preMax: 1,
-					mrgn: 10,
-					supMin: 10,
-					supMax: 100,
-					prov: 1,
-					nHab: 1,
-					clfEn: 1,
-					//stdo: 1,
-					//tpoViv: 1,
-					//carac: 1,
+					filtros,
 				},
 			})
 			.then((result) => {
 				return result.data;
 			});
 		return inmuebles;
-	}
-
-	listadoInmuebles() {
-		let inmuebles = new Catalogo();
-		let listadoInmuebles = inmuebles.getCatalogo();
-		return listadoInmuebles;
 	}
 
 	mostrarInmuebles(catalogo: Promise<any>) {
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,27 @@ import { Catalogo } from './catalogo/index.catalogo';
 import { simuladorHipoteca } from './inmueble/hipoteca/index.hipoteca';
 const mostrarMapa = require('../public/js/mapa.js');
 
+//Creacion de catalogo
 let catalogo: Catalogo = new Catalogo();
-
-let inmuebles = catalogo.listadoInmuebles();
+let filtros: any = {
+	opt: 1,
+	ord: 1,
+	preMin: 1,
+	preMax: 1,
+	mrgn: 10,
+	supMin: 10,
+	supMax: 100,
+	prov: 1,
+	nHab: 1,
+	clfEn: 1,
+};
+let inmuebles = catalogo.getCatalogo(filtros);
 catalogo.mostrarInmuebles(inmuebles);
 
+// Creacion de Hipoteca (ejemplo)
 let precio: number = 300000;
 let localizacion: string = 'Valencia';
 let hipoteca = new simuladorHipoteca(precio, localizacion);
-
 let ahorro = 25000;
 let interes = 'fijo';
 let valorVariable = 0.0;
@@ -21,4 +33,5 @@ let calculoInteres = hipoteca.calculoInteres(interes, valorVariable, calculoAhor
 let total = hipoteca.calculoTotal(calculoInteres, calculoAhorro, anos);
 console.log(total, hipoteca.calculoCuotaMensual(total, anos));
 
+// Mostrar mapa
 mostrarMapa.mostrarMapa(inmuebles);
