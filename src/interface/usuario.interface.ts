a6f129d25c30662c8cf59c43c7672af1949f85bf
Juan <ljuan7397@gmail.com>
2021-04-20 18:50:21 +0200

    comunicacion login y creacion de cookie

--- src/interface/usuario.interface.ts
+++ src/interface/usuario.interface.ts
@@ -1,4 +1,4 @@
 export interface Usuario {
-	username: string;
+	mail: string;
 	password: string;
 }
--- src/login/login.ts
+++ src/login/login.ts
@@ -9,17 +9,25 @@ export class Login {
 	private async postLogin(usuario: Usuario) {
 		const myRequest = 'http://localhost:3000/login';
 		axios.post(myRequest, usuario).then(
-			(response) => console.log(response),
+			(response) => {
+				// if (response.data == true) {
+				// 	this.setCookie('username', usuario.username);
+				// }
+				console.log(response.data);
+			},
 			(error) => console.log(error)
 		);
 	}
 	private isLoggedIn(): boolean {
+		if (this.getCookie('username') !== null) return true;
 		return false;
 	}
 
 	private autoRedirect(): void {}
 
-	private logOut(): void {}
+	private logOut(): void {
+		this.delete_cookie('username');
+	}
 
 	private obtenerParametrosLogin(): void {
 		let loginForm: HTMLFormElement =
@@ -29,11 +37,32 @@ export class Login {
 			let mail = formData.get('user') as string;
 			let pass = formData.get('password') as string;
 			let usuario: Usuario = {
-				username: mail,
+				mail: mail,
 				password: pass,
 			};
 			this.postLogin(usuario);
 			return false;
 		};
+	}
+
+	private setCookie(name: string, value: string): void {
+		let expires = '';
+		let date = new Date();
+		date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000);
+		expires = '; expires=' + date.toUTCString();
+		document.cookie = name + '=' + value + expires + '; path=/';
+	}
+	private getCookie(name: string): string | null {
+		var nameEQ = name + '=';
+		var ca = document.cookie.split(';');
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+		}
+		return null;
+	}
+	private delete_cookie(name: string) {
+		document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 	}
 }
