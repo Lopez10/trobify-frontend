f1453340d700254c940b4029470f29aef0ec70f1
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2021-05-03 20:00:16 +0200

    dynamic filters on search now works

--- public/js/busqueda.js
+++ public/js/busqueda.js
@@ -1,5 +1,9 @@
 export function modificarFiltro(opt, prov, tpoInm) {
+<<<<<<< Updated upstream
 	console.log(tpoInm);
+=======
+	//console.log(opt);
+>>>>>>> Stashed changes
 	let provincia = document.getElementById('provincias');
 	let option1 = document.getElementById('opt1');
 	let option2 = document.getElementById('opt2');
