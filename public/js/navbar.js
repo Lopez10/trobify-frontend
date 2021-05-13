70b3488754c4c53999c887eb38a932a90e6e7684
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2021-05-13 17:49:11 +0200

    cambios navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,3 +1,37 @@
-function logOut() {
+export function logout() {
 	document.cookie = 'mail=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+	window.location.replace("/public/index.html");
+}
+
+export function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+export function checkCookie(cname) {
+  return !getCookie(cname) != "";
+}
+
+export function isLogged() {
+  return checkCookie("mail");
+}
+
+export function setNavbar() {
+  if (isLogged()) {
+		var e = document.querySelectorAll("[logged]");
+		e.forEach( v => {
+			
+		});
+	}
 }
